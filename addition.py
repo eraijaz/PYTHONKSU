558d8c5567242ce2ed88035302e6c4e85c418c78
aijazreshi <107546728+aijazreshi@users.noreply.github.com>
2022-10-29 01:20:51 +0530

    init

---
+++ addition.py
@@ -0,0 +1,10 @@
+x=10
+y=20
+print("Sum = ",x+y)
+# if i  want to data from user i will make use of input() function
+num1=int(input("enter first number :"))
+num2=int(input("enter second number :"))
+print("sum of ",num1," , ",num2 ,"=",num1+num2)
+print("multiplication  of ",num1," , ",num2 ,"=",num1*num2)
+print("subtraction of ",num1," , ",num2 ,"=",num1-num2)
+print("division of ",num1," , ",num2 ,"=",num1/num2)
---
+++ inputFunction.py
@@ -0,0 +1,4 @@
+x=input("Enter your name:-")
+y=input("Enter your car name:-")
+print("hello your name is :-",x)
+print("the name of your car is :",y)
